1d23b49a796d99f0068dbb2c78ee95f03bcef033
max-brwser <max-brwser@max-browser.com>
2022-11-16 15:55:07 +0800

    [Feat] 在無痕下隱藏 "隱私瀏覽功能的常見迷思" 連結

--- app/src/main/java/org/mozilla/fenix/home/sessioncontrol/viewholders/PrivateBrowsingDescriptionViewHolder.kt
+++ app/src/main/java/org/mozilla/fenix/home/sessioncontrol/viewholders/PrivateBrowsingDescriptionViewHolder.kt
@@ -7,15 +7,12 @@ package org.mozilla.fenix.home.sessioncontrol.viewholders
 import android.view.View
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -97,7 +94,9 @@ fun PrivateBrowsingDescription(
                     indication = null,
                     onClickLabel = stringResource(R.string.a11y_action_label_read_article),
                     onClick = onLearnMoreClick,
-                ),
+                    enabled = false,
+                )
+                .alpha(0f),
         ) {
             Text(
                 text = stringResource(R.string.private_browsing_common_myths),
